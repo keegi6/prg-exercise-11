91d647ba18b2129f4ba99069831cac31edb6010f
Michaela Vozakova <michaela.vozakova@gmail.com>
2026-04-20 13:57:32 +0200

    fin

--- sorting.py
+++ sorting.py
@@ -1,11 +1,12 @@
 import random
 import matplotlib.pyplot as plt
 
+
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
 
-def selection_sort(values):
 
+def selection_sort(values):
     print(values)
     for min_index in range(len(values)):
         # print(values)
@@ -17,7 +18,7 @@ def selection_sort(values):
                 min_index = i
         values[min_ind], values[min_index] = values[min_index], values[min_ind]
     print(values)
-print(selection_sort(random_numbers(10)))
+
 
 def bubble_sort(values):
     plt.ion()
@@ -39,9 +40,17 @@ def bubble_sort(values):
                 plt.bar(range(len(values)), values, color=colors)
                 plt.title("Bubble Sort")
                 plt.pause(0.1)
+
         count -= 1
     print(numbers)
     plt.ioff()
     plt.show()
 
-print(bubble_sort(random_numbers(10)))
+
+def main():
+    seznam1 = random_numbers(10, low=0, high=100)
+    selection_sort(seznam1)
+    seznam2 = random_numbers(10, low=0, high=100)
+    bubble_sort(seznam2)
+
+main()
