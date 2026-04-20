7c2bef0ac91b926ebe578b2d3a8c300867508fa0
Michaela Vozakova <michaela.vozakova@gmail.com>
2026-04-20 12:51:58 +0200

    2

--- sorting.py
+++ sorting.py
@@ -2,3 +2,17 @@ import random
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
+
+def selection_sort(values):
+    print(values)
+    for min_index in range(len(values)):
+        # print(values)
+        min_value = values[min_index]
+        min_ind = min_index
+        for i in range(min_index, len(values)):
+            if values[i] < min_value:
+                min_value = values[i]
+                min_index = i
+        values[min_ind], values[min_index] = values[min_index], values[min_ind]
+    print(values)
+print(selection_sort(random_numbers(10)))
